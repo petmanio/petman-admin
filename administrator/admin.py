ce9600c98110ccd8386d6cf2609c6afc88ece389
andantonyan <andantonyan@gmail.com>
2017-04-11 16:32:53 +0400

    feat(*): add static files, change textarea row size, etc

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import TextField
+from django.forms import Textarea
+
 from .models import Blog, Petcare, Petcarecategory, PetcareCategoriesPetcarecategoryOwners
 
 @admin.register(Blog)
@@ -7,11 +10,15 @@ class BlogAdmin(admin.ModelAdmin):
 
 @admin.register(Petcare)
 class PetCareAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows': 1})},
+    }
 
 @admin.register(Petcarecategory)
 class PetCareCategoryAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows': 1})},
+    }
 
 # @admin.register(PetcareCategoriesPetcarecategoryOwners)
 # class PetcareCategoriesPetcarecategoryOwnersAdmin(admin.ModelAdmin):
